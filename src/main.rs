3dc992b8ae569176522b1c90ad2adcf1b3a3d759
John Naylor <jonaylor89@gmail.com>
2019-12-23 14:39:57 -0500

    Add minor minor error checking

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ use br4infuck::evaluate;
 fn main() {
     let path = match env::args().nth(1) {
         Some(x) => x,
-        _ => return
+        _ => {
+            println!("[-] Must specify a path to a file containing brainfuck code");
+            return
+        }
     };
 
     let code_string = match fs::read_to_string(path) {
         Ok(x) => x,
-        _ => return
+        _ => {
+            println!("[-] Unable to read in file");
+            return
+        }
     };
 
     evaluate(code_string);
